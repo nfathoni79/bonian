0b38d00881053b3a542e09013e9af0e9e3d5c58c
kinosuke <iwaninfo@gmail.com>
2019-02-11 13:36:47 +0700

    added method cost and php docs

--- src/Controller/Component/RajaOngkirComponent.php
+++ src/Controller/Component/RajaOngkirComponent.php
@@ -44,16 +44,27 @@ class RajaOngkirComponent extends Component
 
     }
 
+    /**
+     * @return mixed
+     */
     protected function getbase()
     {
         return $this->defaultRequest['path'];
     }
 
+    /**
+     * @param null $url
+     * @return string
+     */
     protected function setPath($url = null)
     {
         return rtrim($this->getbase(), '/') . $url;
     }
 
+    /**
+     * @param null $id
+     * @return mixed
+     */
     public function getProvince($id = null)
     {
         $http = new Client($this->defaultRequest);
@@ -64,6 +75,11 @@ class RajaOngkirComponent extends Component
         }
     }
 
+    /**
+     * @param null $province_id
+     * @param null $city_id
+     * @return mixed
+     */
     public function getCity($province_id = null, $city_id = null)
     {
         $http = new Client($this->defaultRequest);
@@ -74,11 +90,59 @@ class RajaOngkirComponent extends Component
         }
     }
 
+
+    /**
+     * @param null $city_id
+     * @param null $id
+     * @return mixed
+     */
     public function getSubdistrict($city_id = null, $id = null)
     {
         $http = new Client($this->defaultRequest);
         $query = isset($city_id) || isset($id) ? ['city' => $city_id, 'id' => $id] : null;
         $response = $http->get($this->setPath('/subdistrict'), $query);
+        if ($response->getStatusCode() == 200) {
+            return json_decode($response->getBody()->getContents(), true);
+        }
+    }
+
+    /**
+     * Method “cost” digunakan untuk mengetahui tarif pengiriman (ongkos kirim) dari dan ke kecamatan tujuan tertentu
+     * dengan berat tertentu pula.
+     * courier code jne, pos, tiki, rpx, esl, pcp, pandu, wahana, sicepat, jnt, pahala, cahaya, sap, jet,
+     * indah, dse, slis, first, ncs, star, ninja, lion, idl.
+     *
+     * @param $origin integer
+     * @param $origin_type string
+     * @param $destination integer
+     * @param $destination_type string
+     * @param $courier string bulk using jne:pos:tiki
+     * @param $weight integer
+     * @param null $length
+     * @param null $height
+     * @param null $width
+     * @return mixed
+     */
+    public function cost($origin, $origin_type, $destination, $destination_type, $courier, $weight, $length = null, $height = null, $width = null)
+    {
+        $http = new Client($this->defaultRequest);
+        $request = [
+            'origin' => $origin,
+            'originType' => $origin_type,
+            'destination' => $destination,
+            'destinationType' => $$destination_type,
+            'weight' => $weight
+        ];
+
+        if (!empty($length) && !empty($height) && !empty($width)) {
+            $request += [
+                'length' => $length,
+                'height' => $height,
+                'width' => $width
+            ];
+        }
+
+        $response = $http->post($this->setPath('/cost'), $request);
         if ($response->getStatusCode() == 200) {
             return json_decode($response->getBody()->getContents(), true);
         }
