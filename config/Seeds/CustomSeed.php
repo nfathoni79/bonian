31c4a3359a26c4d8fbcbaa3f2ab56ba168712089
kinosuke <iwaninfo@gmail.com>
2019-03-05 15:27:23 +0000

    costum seed prepare API

--- config/Seeds/CustomSeed.php
+++ config/Seeds/CustomSeed.php
@@ -8,6 +8,34 @@ class CustomSeed extends AbstractSeed
     {
          $this->call('AttributesSeed'); 
          $this->call('BranchesSeed'); 
+         $this->call('TagsSeed'); 
+         $this->call('ProductToCourriersSeed'); 
+         $this->call('ProductToCategoriesSeed'); 
+         $this->call('ProductTagsSeed'); 
+         $this->call('ProductStockMutationTypesSeed'); 
+         $this->call('ProductStatusesSeed'); 
+         $this->call('ProductOptionValueListsSeed'); 
+         $this->call('ProductOptionStocksSeed'); 
+         $this->call('ProductOptionPricesSeed'); 
+         $this->call('ProductImageSizesSeed'); 
+         $this->call('ProductImagesSeed'); 
+         $this->call('ProductCategoriesSeed'); 
+         $this->call('ProductAttributesSeed'); 
+         $this->call('ProductsSeed'); 
+         $this->call('OptionValuesSeed'); 
+         $this->call('BrandsSeed'); 
+         $this->call('BranchesSeed'); 
+         $this->call('AttributesSeed'); 
+         $this->call('ProductWarrantiesSeed'); 
+         $this->call('GameWheelsSeed'); 
+         $this->call('OptionsSeed'); 
+         $this->call('SubdistrictsSeed'); 
+         $this->call('ProvincesSeed'); 
+         $this->call('ProductStockStatusesSeed'); 
+         $this->call('CourriersSeed'); 
+         $this->call('CitiesSeed'); 
+         $this->call('ProductWeightClassesSeed'); 
+         $this->call('OptionTypesSeed'); 
 
     }
 }
